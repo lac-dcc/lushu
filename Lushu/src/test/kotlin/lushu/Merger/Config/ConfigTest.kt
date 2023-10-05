6cbf1c378c47378c7b9d776460d970cf449448d9
Alex H <48073242+Yowgf@users.noreply.github.com>
2023-10-05 13:48:26 -0500

    Add CI (#15)

--- Lushu/src/test/kotlin/lushu/Merger/Config/ConfigTest.kt
+++ Lushu/src/test/kotlin/lushu/Merger/Config/ConfigTest.kt
@@ -7,22 +7,6 @@ import org.junit.jupiter.api.Test
 
 class ConfigTest {
     @Test
-    fun configEmptyShouldRaise() {
-        assertThrows(
-            MismatchedInputException::class.java,
-            { Config.fromConfigFile(Utils.configFullPath("configEmpty.yaml")) }
-        )
-    }
-
-    @Test
-    fun configMalformedShouldRaise() {
-        assertThrows(
-            MismatchedInputException::class.java,
-            { Config.fromConfigFile(Utils.configFullPath("configMissingRequired.yaml")) }
-        )
-    }
-
-    @Test
     fun configWellFormedShouldNotRaise() {
         Config.fromConfigFile(Utils.basicConfigFullPath())
     }
