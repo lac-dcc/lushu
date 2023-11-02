77499c4adca206bf6e2ee8b0a93344229cb9e6d6
yowgf <alexthomasmol@gmail.com>
2023-11-01 22:45:59 -0500

    Initial soft refactoring

--- Lushu/src/main/kotlin/lushu/ContextGrammar/MapGrammar/MapGrammar.kt
+++ Lushu/src/main/kotlin/lushu/ContextGrammar/MapGrammar/MapGrammar.kt
@@ -8,18 +8,20 @@ import java.io.FileReader
 
 class MapGrammar(
     val dsl: DSL = DSL(),
-    var max_blocks: Int = 0,
+    var maxBlocks: Int = 0,
 ) {
     data class PivotData(
-        val opening_reference: Token,
-        val lambda_funcion: String,
+        val openingRef: Token,
+        val func: String,
     )
+
+    private val merger = MergerS.merger()
 
     private lateinit var outfile: BufferedWriter
 
-    private val opening_map: MutableMap<Token, Int> = mutableMapOf()
-    private val closing_map: MutableMap<Token, Token> = mutableMapOf()
-    private val map_pivot: MutableMap<Token, PivotData> = mutableMapOf()
+    private val openingMap: MutableMap<Token, Int> = mutableMapOf()
+    private val closingMap: MutableMap<Token, Token> = mutableMapOf()
+    private val mapPivot: MutableMap<Token, PivotData> = mutableMapOf()
 
     private fun string2list(string: String): List<String> {
         val lines = string.split("\n")
@@ -62,43 +64,38 @@ class MapGrammar(
     }
 
     private fun addAccMap(token: Token, value: Int) {
-        val oldValue = opening_map[token] ?: 0
-        this.opening_map[token] = oldValue + value
+        val oldValue = openingMap[token] ?: 0
+        this.openingMap[token] = oldValue + value
     }
 
     private fun match(word: String) {
-        val inToken = MergerS.merger().tokensFromString(word)
-        val openingFound = inMap(opening_map, inToken)
+        val inToken = merger.tokensFromString(word)
+        val openingFound = inMap(openingMap, inToken)
         if (openingFound != null) {
             addAccMap(openingFound, 1)
         }
 
-        val closingFound = inMap(closing_map, inToken)
+        val closingFound = inMap(closingMap, inToken)
         if (closingFound != null) {
-            val openingToken = closing_map[closingFound]
+            val openingToken = closingMap[closingFound]
             if (openingToken != null) {
                 addAccMap(openingToken, -1)
             }
         }
 
-        map_pivot.forEach { pivot ->
+        mapPivot.forEach { pivot ->
             if (pivot.component1().match(inToken)) {
-                val openingReference = pivot.component2().opening_reference
-                val value = opening_map[openingReference]
+                val openingReference = pivot.component2().openingRef
+                val value = openingMap[openingReference]
                 if (value != null && value > 1) {
-                    max_blocks += 1
+                    maxBlocks += 1
                 }
             }
         }
     }
 
     private fun streamString(input: String) {
-        val wordRegex = "\\S+".toRegex()
-
-        wordRegex.findAll(input).forEach { matchResult ->
-            val word = matchResult.value
-            match(word)
-        }
+        input.split(spaceDelim).forEach { word -> match(word) }
     }
 
     fun consume(input: String) {
@@ -111,12 +108,11 @@ class MapGrammar(
                 consume(it)
             }
         }
-        return max_blocks
+        return maxBlocks
     }
 
     private fun extractContext(input: String): List<String> {
-        val regex = Regex("<c>(.*?)</c>")
-        val matcher = regex.findAll(input)
+        val matcher = contextRegex.findAll(input)
         val substrings = matcher.map { it.groupValues[1] }.toList()
         return substrings
     }
@@ -142,8 +138,8 @@ class MapGrammar(
 
     private fun insertPivot(pivots: List<String>, openingToken: Token) {
         pivots.forEach { pivot ->
-            val lambFun = getLambdaFunction(pivot)
-            insert(map_pivot, pivot, PivotData(openingToken, lambFun))
+            val func = getLambdaFunction(pivot)
+            insert(mapPivot, pivot, PivotData(openingToken, func))
         }
     }
 
@@ -159,18 +155,18 @@ class MapGrammar(
     private fun insertContext(context: String) {
         val contextTokens = string2list(context)
 
-        if (contextTokens.size < min_elements) {
+        if (contextTokens.size < minElems) {
             println("Warning: Insufficient number of elements provided.")
             return
         }
 
         val token = toToken(contextTokens.first())
-        val res = findEquivalent(opening_map, token)
+        val res = findEquivalent(openingMap, token)
         val openingToken = res.first
         if (!res.second) {
-            insert(opening_map, openingToken, no_occurrences)
+            insert(openingMap, openingToken, noOccurrences)
         }
-        insert(closing_map, contextTokens.last(), openingToken)
+        insert(closingMap, contextTokens.last(), openingToken)
         insertPivot(contextTokens.subList(1, contextTokens.size - 1), openingToken)
     }
 
@@ -183,9 +179,12 @@ class MapGrammar(
     }
 
     companion object {
-        val min_elements = 3
-        val no_occurrences = 0
-        val space_delimiter = " "
-        val newline_delimiter = "\n"
+        val minElems = 3
+        val noOccurrences = 0
+        val spaceDelim = " "
+        val newlineDelim = "\n"
+
+        val contextRegex = Regex("""<c>(.*?)</c>""")
+        val wordRegex = Regex("""\S+""")
     }
 }
